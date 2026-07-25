88787ac0c9708ab39fa549dc31fdce11d8c1a2bf
Olivier <olivier@MacBook-Pro-de-Olivier.local>
2026-07-25 18:48:00 +0200

    Setup Auth.js with Drizzle adapter

---
+++ src/auth.ts
@@ -0,0 +1,15 @@
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+
+import { db } from "@/db/client";
+
+export const {
+  handlers,
+  signIn,
+  signOut,
+  auth,
+} = NextAuth({
+  adapter: DrizzleAdapter(db),
+
+  providers: [],
+});
